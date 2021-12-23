ea1fb7069040f2c92d2ecad5d031b5a4883064fb
chanki <chanki.kim89@gmail.com>
2021-12-23 14:43:37 +0900

    서비스워커삭제

--- src/index.js
+++ src/index.js
@@ -13,18 +13,6 @@ const authService = new AuthService();
 const postRepository = new PostRepository(firebaseApp);
 const imageUploader = new ImageUploader();
 
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker
-            .register("/sw.js")
-            .then(registration => {
-                console.log("SW registered: ", registration);
-            })
-            .catch(registrationError => {
-                console.log("SW registration failed: ", registrationError);
-            });
-    });
-}
 ReactDOM.render(
     <App
         authService={authService}
